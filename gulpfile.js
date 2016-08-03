849c7f434480d56cbaacdd6d5ff605a73550a736
Manuel Abrante <mabrante@socialh4ck.com>
2016-08-03 19:21:03 -0400

    create gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var stylus = require('gulp-stylus');
+var minify = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var flatten = require('gulp-flatten')
+var nib = require('nib');
+var browserSync = require('browser-sync');
+
+
+/* Clean task*/
+gulp.task('clean', function() {
+  return gulp.src('build/', { read: false })
+    .pipe(clean());
+});
+
+/* Build CSS */
+gulp.task('stylus', function() {
+  return gulp.src('src/assets/css/' + 'app.styl')
+    .pipe(stylus({
+      use: [nib()]
+    }))
+    .pipe(rename({
+      extname: '.min.css'
+    }))
+    .pipe(minify())
+    .pipe(gulp.dest('build/assets/css/'))
+});
+
+/* Concat repo JS*/
+gulp.task('appScripts', function() {
+  return gulp.src('src/js/' + '**/*.js')
+    .pipe(concat('app.min.js'))
+    .pipe(uglify({ mangle: false }))
+    .pipe(gulp.dest('build/js/'));
+});
+
+/* Load templates */
+gulp.task('templates', function() {
+  return gulp.src('src/templates' + '*/*.html')
+    .pipe(flatten())
+    .pipe(gulp.dest('build'));
+});
+
+/* Load Images */
+gulp.task('images', function() {
+  return gulp.src('app/assets/img/' + '*')
+    .pipe(gulp.dest('build/assets/img'))
+});
+
+/* Server init */
+gulp.task('browserSync', function(){
+  browserSync({
+    host: 'localhost',
+    port: 3000,
+    // open: 'external',
+    server: {
+      baseDir: 'build/'
+    }
+  })
+});
+
+
+/* Build */
+gulp.task('build', [
+  'stylus',
+  'images',
+  'appScripts',
+  'templates',
+]);
+
+/* Init app*/
+gulp.task('default', ['clean'], function(){
+  gulp.start('build');
+  gulp.start('browserSync');
+});
