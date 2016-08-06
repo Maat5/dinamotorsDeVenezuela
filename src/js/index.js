ef6615ca7c6b53e865732e7868e8947d8b0d6a95
Manuel Abrante <abrantemanuel@gmail.com>
2016-08-06 10:16:38 -0400

    load templates with jquery on document onReady

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,23 @@
 var $ = require('jquery');
 $('h1').html('Dinamotors de Venezuela');
+
+$(document).ready(function(){
+
+  //load header template
+  $('header').load('header.html');
+
+  //load nav menu template
+  $('#nav').load('nav.html');
+
+  //load gallery template
+  $('#gallery').load('gallery.html');
+
+  //load services template
+  $('#services').load('services.html');
+
+  //load contact template
+  $('#contact').load('contact.html');
+
+  //load footer template
+  $('footer').load('footer.html');
+});
