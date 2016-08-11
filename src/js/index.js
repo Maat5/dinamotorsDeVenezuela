84253809007953b9df68e1dc13147edfbda0fff4
Cindy Salinas <cindysalinas15@gmail.com>
2016-08-10 20:25:32 -0400

    fixed templates

--- src/js/index.js
+++ src/js/index.js
@@ -2,56 +2,31 @@ var $ = require('jquery');
 $('h1').html('Dinamotors de Venezuela');
 
 $(document).ready(function(){
+  var slides = document.querySelectorAll('#slides .slide');
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide,2000);
 
-  //load header template
-  $('header').load('header.html');
+  function nextSlide(){
+      goToSlide(currentSlide+1);
+  }
 
-  //load nav menu template
-  $('#nav').load('nav.html');
+  function previousSlide(){
+      goToSlide(currentSlide-1);
+  }
 
-  //load gallery template
-  $('#gallery').load('gallery.html');
+  function goToSlide(n){
+      slides[currentSlide].className = 'slide';
+      currentSlide = (n+slides.length)%slides.length;
+      slides[currentSlide].className = 'slide showing';
+  }
 
-  // load company template
-  $('#company').load('company.html');
+  var next = document.getElementById('next');
+  var previous = document.getElementById('previous');
 
-  //load services template
-  $('#services').load('services.html');
-
-  //load location template
-  $('#location').load('location.html');
-
-  //load contact template
-  $('#contact').load('contact.html');
-
-  //load footer template
-  $('footer').load('footer.html');
+  next.onclick = function(){
+      nextSlide();
+  };
+  previous.onclick = function(){
+      previousSlide();
+  };
 });
-
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
-
-function nextSlide(){
-    goToSlide(currentSlide+1);
-}
-
-function previousSlide(){
-    goToSlide(currentSlide-1);
-}
-
-function goToSlide(n){
-    slides[currentSlide].className = 'slide';
-    currentSlide = (n+slides.length)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
-
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
-
-next.onclick = function(){
-    nextSlide();
-};
-previous.onclick = function(){
-    previousSlide();
-};
