050ee7c47779ed9cbe20942163d59da826d15a4b
Keily Salinas <keisalinas2@gmail.com>
2016-08-10 20:11:53 -0400

    added slider functions

--- src/js/index.js
+++ src/js/index.js
@@ -27,3 +27,31 @@ $(document).ready(function(){
   //load footer template
   $('footer').load('footer.html');
 });
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide(){
+    goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+    goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+next.onclick = function(){
+    nextSlide();
+};
+previous.onclick = function(){
+    previousSlide();
+};
