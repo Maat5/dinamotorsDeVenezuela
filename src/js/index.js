12e86ef558d432300245e36321fe02d630aedb45
Manuel Abrante <abrantemanuel@gmail.com>
2016-08-14 12:09:43 -0400

    change slider interval

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ $('h1').html('Dinamotors de Venezuela');
 $(document).ready(function(){
   var slides = document.querySelectorAll('#slides .slide');
   var currentSlide = 0;
-  var slideInterval = setInterval(nextSlide,2000);
+  var slideInterval = setInterval(nextSlide,4000);
 
   function nextSlide(){
       goToSlide(currentSlide+1);
