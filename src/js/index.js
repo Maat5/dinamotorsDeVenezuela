17fbb87576ce98253910f61a03f81daf134f37b5
Keily Salinas <keisalinas2@gmail.com>
2016-08-15 18:05:15 -0400

    update slide functions

--- src/js/index.js
+++ src/js/index.js
@@ -7,27 +7,24 @@ $(document).ready(function(){
   var slideInterval = setInterval(nextSlide,4000);
 
   function nextSlide(){
-      goToSlide(currentSlide+1);
+    goToSlide(currentSlide+1);
   }
 
   function previousSlide(){
-      goToSlide(currentSlide-1);
+    goToSlide(currentSlide-1);
   }
 
   function goToSlide(n){
-      slides[currentSlide].className = 'slide';
-      currentSlide = (n+slides.length)%slides.length;
-      slides[currentSlide].className = 'slide showing';
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
   }
 
-  var next = document.getElementById('next');
-  var previous = document.getElementById('previous');
-
-  next.onclick = function(){
-      nextSlide();
+  $('.next').click(function(){
+    nextSlide();
   };
-  previous.onclick = function(){
-      previousSlide();
+  $('.previous').click(function(){
+    previousSlide();
   };
 
   $('.toggle-menu').on('click', function(){
